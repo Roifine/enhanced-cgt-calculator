b23ed5a0fe5e604f17ddfae1a2c99ebe3c54615e
Roi Fine <fineroi@gmail.com>
2025-06-22 15:31:41 +1000

    test

--- app.py
+++ app.py
@@ -295,7 +295,7 @@ def main():
     track_event("calculator_page_view", "user_journey", "streamlit_app")  # ← ADD THIS
     
     # Header
-    st.markdown("<h1 style='text-align: center;'>🧮 Shares Tax Calculator</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>Shares Tax Calculator</h1>", unsafe_allow_html=True)
     st.markdown("<h4 style='text-align: center;'>Smart, secure, and simple", unsafe_allow_html=True)
     st.markdown("---")
     
